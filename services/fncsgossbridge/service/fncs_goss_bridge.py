00d49849803258635939d3507606e33b5157f5b6
Tara Gibson <tara@pnnl.gov>
2021-01-25 14:23:29 -0800

    Pass system username/pw into fncsgossbridge when launching service

--- services/fncsgossbridge/service/fncs_goss_bridge.py
+++ services/fncsgossbridge/service/fncs_goss_bridge.py
@@ -666,21 +666,21 @@ def _publish_to_fncs_bus(simulation_id, goss_message, command_filter):
             }
             if x.get("attribute", "") != "IdentifiedObject.Fault":
                 if command_pair not in command_filter:
-                    object_name = (object_mrid_to_name.get(x.get("object"))).get("name")
+                    object_name = (object_mrid_to_name.get(x.get("object",{}))).get("name")
                     # _send_simulation_status("ERROR", "Jeff1 " + object_name, "ERROR")
-                    object_phases = (object_mrid_to_name.get(x.get("object"))).get("phases")
+                    object_phases = (object_mrid_to_name.get(x.get("object",{}))).get("phases")
                     # _send_simulation_status("ERROR", "Jeff2 " + object_phases, "ERROR")
-                    object_total_phases = (object_mrid_to_name.get(x.get("object"))).get("total_phases")
+                    object_total_phases = (object_mrid_to_name.get(x.get("object",{}))).get("total_phases")
                     # _send_simulation_status("ERROR", "Jeff3 " + object_total_phases, "ERROR")
-                    object_type = (object_mrid_to_name.get(x.get("object"))).get("type")
+                    object_type = (object_mrid_to_name.get(x.get("object",{}))).get("type")
                     # _send_simulation_status("ERROR", "Jeff4 " + object_type + " " + x.get("attribute"), "ERROR")
-                    object_name_prefix = ((difference_attribute_map.get(x.get("attribute"))).get(object_type)).get("prefix")
+                    object_name_prefix = ((difference_attribute_map.get(x.get("attribute",{}))).get(object_type,{})).get("prefix")
                     # _send_simulation_status("ERROR", "Jeff5 " + object_name_prefix, "ERROR")
                     cim_attribute = x.get("attribute")
         
-                    object_property_list = ((difference_attribute_map.get(x.get("attribute"))).get(object_type)).get("property")
+                    object_property_list = ((difference_attribute_map.get(x.get("attribute",{}))).get(object_type,{})).get("property")
                     # _send_simulation_status("ERROR", "Jeff6 " + str(object_property_list), "ERROR")
-                    phase_in_property = ((difference_attribute_map.get(x.get("attribute"))).get(object_type)).get("phase_sensitive",False)
+                    phase_in_property = ((difference_attribute_map.get(x.get("attribute",{}))).get(object_type,{})).get("phase_sensitive",False)
                     # _send_simulation_status("ERROR", "Jeff7 " + str(phase_in_property), "ERROR")
                     if (object_name_prefix + object_name) not in fncs_input_message["{}".format(simulation_id)].keys():
                         fncs_input_message["{}".format(simulation_id)][object_name_prefix + object_name] = {}
@@ -999,7 +999,7 @@ def _done_with_time_step(current_time):
         _send_simulation_status('ERROR', message_str, 'ERROR')
 
 
-def _register_with_goss(sim_id,username,password,goss_server='localhost',
+def _register_with_goss(sim_id,user,passwd,goss_server='localhost',
                       stomp_port='61613', sim_duration=86400, sim_start=0):
     """Register with the GOSS server broker and return.
 
@@ -1039,8 +1039,8 @@ def _register_with_goss(sim_id,username,password,goss_server='localhost',
     #goss_connection = stomp.Connection12([(goss_server, stomp_port)])
     #goss_connection.start()
     #goss_connection.connect(username,password, wait=True)
-    goss_connection = GridAPPSD(simulation_id, (goss_server, stomp_port),
-                                username, password)
+    goss_connection = GridAPPSD(simulation_id, address=([(goss_server, stomp_port)]),
+                                username=user, password=passwd)
     #goss_connection.set_listener('GOSSListener', goss_listener_instance)
     #goss_connection.subscribe(input_from_goss_topic,1)
     #goss_connection.subscribe(simulation_input_topic + "{}".format(simulation_id),2)
@@ -1411,10 +1411,10 @@ def _keep_alive(is_realtime, archive_db_file, archive_file, only_archive):
 
 
 def _main(simulation_id, simulation_broker_location='tcp://localhost:5570', measurement_map_dir='', is_realtime=True,
-          sim_duration=86400, sim_start=0, archive_db_file=None, archive_file=None, only_archive=False):
-    
+          sim_duration=86400, sim_start=0, archive_db_file=None, archive_file=None, only_archive=False, username='system', password='manger'):
+    print("STARTING BRIDGE WITH "+username+" "+password)
     measurement_map_file=str(measurement_map_dir)+"model_dict.json"
-    _register_with_goss(simulation_id,'system','manager','127.0.0.1','61613', sim_duration, sim_start)
+    _register_with_goss(simulation_id,username,password,'127.0.0.1','61613', sim_duration, sim_start)
     _register_with_fncs_broker(simulation_broker_location)
     _create_cim_object_map(measurement_map_file)
     _keep_alive(is_realtime, archive_db_file, archive_file, only_archive)
@@ -1425,6 +1425,8 @@ def _get_opts():
     parser.add_argument("broker_location", help="The location of the FNCS broker.")
     parser.add_argument("simulation_directory", help="The simulation files directory.")
     parser.add_argument("simulation_request", help="The simulation request.")
+    parser.add_argument("username", help="The username to connect to the message bus.")
+    parser.add_argument("password", help="The password to connect to the message bus.")
     parser.add_argument("logLevel", help="The log level at which platform was started")
     opts = parser.parse_args()
     return opts
@@ -1461,5 +1463,6 @@ if __name__ == "__main__":
         archive_file = "/tmp/gridappsd_tmp/{simulation_id}/archive.tar.gz".format(simulation_id=simulation_id)
     _log.debug("Archive settings:\n\tarchive_db_file: {}\n\tarchive_file: {}\n\tonly_archive: {}".format(
         archive_db_file, archive_file, only_archive))
+	print("ABOUTY TO CALL MAIN WITH USERNAME "+username+" "+password)
     _main(simulation_id, sim_broker_location, sim_dir, run_realtime, sim_duration, sim_start_str,
-          archive_db_file, archive_file, only_archive)
+          archive_db_file, archive_file, only_archive, username, password)
